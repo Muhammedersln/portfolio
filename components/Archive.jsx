493fda802364788a2c40ab1f4b8bce2d6904265c
Muhammed Eraslan <muhammed.ersln@icloud.com>
2024-08-16 22:34:46 +0300

    update archive-cv

--- components/Archive.jsx
+++ components/Archive.jsx
@@ -14,10 +14,16 @@ const Archive = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10'>
                 <ArchiveCard
-                    title="Unity RunController"
-                    des="Unity oyun moturu ile geliştirdiğim Hyper Casual platform oyunu. Oyunun amacı, karakteri engellerden kaçırarak maksimum skor yapmaktır."
-                    listItems={['Unity', 'C#', 'Blender']}
-                    link="https://github.com/Muhammedersln/Unity-RunController"
+                    title="ShortLink"
+                    des=".Net Core ile geliştirdiğim bu proje ile istediğiniz bir linki kısaltabilir ve yönetebilirsiniz. Kullanıcılar linklerini kaydedebilir ve yönetebilir."
+                    listItems={['.NET', 'MVC', 'Web API']}
+                    link="https://github.com/Muhammedersln/ShortLink"
+                />
+                <ArchiveCard
+                    title="EraslanMedia Web Digital MarketPlace"
+                    des="React ve Next.js v14 ile oluşturulmuş, verimli API iletişimi için tRPC'den ve modern ve duyarlı bir tasarım için Tailwind CSS'den yararlanan kapsamlı bir dijital pazar projesi."
+                    listItems={['React', 'Tailwind', 'tRPC']}
+                    link="https://github.com/Muhammedersln/EraslanMedia-web-digital-market"
                 />
                 <ArchiveCard
                     title="Mami Flix"
@@ -27,16 +33,16 @@ const Archive = () => {
                     link="https://github.com/Muhammedersln/MamiFlix"
                 />
                 <ArchiveCard
-                    title="Java Scrum"
-                    des="Java ile geliştirdiğim bu projede scrum, proje yönetimi ve takım yönetimi üzerine bir uygulama geliştirdim."
-                    listItems={['Java', 'JavaFX', 'Mysql']}
-                    link="https://github.com/Muhammedersln/javaMysql"
-                />
-                <ArchiveCard
                     title="Portfolio"
                     des="Bu portfolyo sitesi, React.js ve Tailwind Css ile geliştirilmiştir. Next.js ile SSR özelliği eklenmiştir. Şu an gördüğünüz site bu projedir."
                     listItems={['React', 'Tailwind', 'Next.js']}
                     link="https://www.muhammederaslan.com"
+                />
+                <ArchiveCard
+                    title="Unity RunController"
+                    des="Unity oyun moturu ile geliştirdiğim Hyper Casual platform oyunu. Oyunun amacı, karakteri engellerden kaçırarak maksimum skor yapmaktır."
+                    listItems={['Unity', 'C#', 'Blender']}
+                    link="https://github.com/Muhammedersln/Unity-RunController"
                 />
                 <ArchiveCard
                     title="Hava Durumu"
@@ -44,15 +50,21 @@ const Archive = () => {
                     listItems={['React', 'Tailwind', 'Next.js']}
                     link="https://weather-app-sooty-eta.vercel.app/"
                 />
-                <ArchiveCard
-                    title="Github Arama Motoru"
-                    des="Github üzerinde arama yaparak kullanıcıları ve repoları listeleyen bir uygulama. React.js ve Tailwind Css ile geliştirilmiştir."
-                    listItems={['React', 'Next.js']}
-                    link="https://github.com/Muhammedersln/Github-Search"
-                />
                 {
                     show && (
                         < >
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                whileInView={{ opacity: 1 }}
+                                transition={{ duration: 0.1 }}
+                            >
+                                <ArchiveCard
+                                    title="CourseApp with .NET"
+                                    des=".NET ile basit düzeyde kurs-öğrenci ilişkisine sahip kurs uygulaması"
+                                    listItems={['.NET', 'C#', 'Identity']}
+                                    link="https://github.com/Muhammedersln/CourseAppwithdotNET"
+                                />
+                            </motion.div>
                             <motion.div
                                 initial={{ opacity: 0 }}
                                 whileInView={{ opacity: 1 }}
@@ -75,6 +87,30 @@ const Archive = () => {
                                     des=".Net Core ile basit bir toplantı uygulaması geliştirdim. Bu uygulama, toplantı oluşturma, katılımcı ekleme işlemlerini yapar."
                                     listItems={['.Net', 'C#', 'MVC']}
                                     link="https://github.com/Muhammedersln/BasicMeetingApp"
+                                />
+                            </motion.div>
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                whileInView={{ opacity: 1 }}
+                                transition={{ duration: 0.1 }}
+                            >
+                                <ArchiveCard
+                                    title="Github Arama Motoru"
+                                    des="Github üzerinde arama yaparak kullanıcıları ve repoları listeleyen bir uygulama. React.js ve Tailwind Css ile geliştirilmiştir."
+                                    listItems={['React', 'Next.js']}
+                                    link="https://github.com/Muhammedersln/Github-Search"
+                                />
+                            </motion.div>
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                whileInView={{ opacity: 1 }}
+                                transition={{ duration: 0.1 }}
+                            >
+                                <ArchiveCard
+                                    title="Java Scrum"
+                                    des="Java ile geliştirdiğim bu projede scrum, proje yönetimi ve takım yönetimi üzerine bir uygulama geliştirdim."
+                                    listItems={['Java', 'JavaFX', 'Mysql']}
+                                    link="https://github.com/Muhammedersln/javaMysql"
                                 />
                             </motion.div>
 
